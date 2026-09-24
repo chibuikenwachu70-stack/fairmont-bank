ee04d8e15e8e0992c41cc28470c362d8a8a76dcc
Chibuike Nwachukwu <worldlinkcourierservice78@gmail.com>
2026-09-24 17:23:40 +0300

    Connect production app to PostgreSQL

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ app.config["SESSION_PERMANENT"] = True
 
 os.makedirs(app.instance_path, exist_ok=True)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = (
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
     "sqlite:///" + os.path.join(app.instance_path, "fairmont.db")
 )
 
@@ -6699,5 +6700,6 @@ if __name__ == "__main__":
     app.run(
     debug=False
 )
+
 
 
